51fd4fc4a24a4c36deb4093b4a8eb920e2c44c51
Nick1091 <nick10.91@mail.ru>
2022-06-03 18:44:09 +0300

    feat: add solving to hash folder

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import { createHash } from 'crypto';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const fileToRead = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const file = await fs.readFile(fileToRead)
+
+    const hash = (string) => {
+        return createHash('sha256').update(string).digest('hex');
+    }
+
+    console.log(hash(file));
 };
+
+calculateHash()
