54d30daf91302223f7c957cf7205f8365244c870
Nick1091 <nick10.91@mail.ru>
2022-06-04 14:11:00 +0300

    feat: add solving for wt folder

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,5 @@
 export const parseEnv = () => {
-    console.log(...process.argv.filter((args) => args.includes('RSS_') ))
+    console.log(process.argv.filter((args) => args.includes('RSS_') ).join('; '))
 };
 
 parseEnv();
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,29 @@
+import { Worker, isMainThread } from "worker_threads"; 
+import { cpus } from 'os';
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const workerPath = path.join(__dirname, 'worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
-};
+        let number = 10;
+        const arrWorkers = [];
+        for(let i = 0; i < cpus().length; i++){
+            arrWorkers.push(
+                new Promise((resolve) => {
+                    new Worker(workerPath, { workerData: number + i })
+                    .on('message', result => {
+                        resolve({ data: result, status: 'resolved'});
+                    })
+                    .on('error', error => {
+                        resolve( { data: null, status: 'error'});
+                    })                 
+                })
+            )
+        }
+        const result = await Promise.all(arrWorkers);
+            console.log(result);
+            return result;
+    }
+    await performCalculations()
